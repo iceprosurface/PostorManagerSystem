2bb4538e848b7feb32a48b879dfa1775357b57ec
iceprosurface <775846939@qq.com>
2016-01-03 15:48:06 +0800

    add new model
    add new model admin
    fix and integrate some URL probleam
    * /index/home/ -> _ENTER_
    * /index/admin/ -> _ADMIN_

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,21 +2,28 @@
 return array(
 	//'配置项'=>'配置值'
 		// 数据库配置信息
-		'DB_TYPE' => 'mysql', // 数据库类型
-		'DB_HOST' => 'localhost', // 服务器地址
-		'DB_NAME' => 'postor', // 数据库名
-		'DB_USER' => 'root', // 用户名
-		'DB_PWD' => '', // 密码
-		'DB_PORT' => 3306, // 端口
-		'DB_PREFIX' => '', // 数据库表前缀
-		'DB_CHARSET'=> 'utf8', // 字符集
-		'DB_FIELDS_CACHE'=>false,
-		'READ_DATA_MAP'=>true,
-		'DEFAULT_FILTER'=>'strip_tags',
-		'API_KEY'=>'',//设置短信平台的验证码
+		'DB_TYPE'			=>'mysql', 		// 数据库类型
+		'DB_HOST'			=>'localhost', 	// 服务器地址
+		'DB_NAME'			=>'postor', 	// 数据库名
+		'DB_USER'			=>'root', 		// 用户名
+		'DB_PWD'			=>'', 			// 密码
+		'DB_PORT'			=>3306, 		// 端口
+		'DB_PREFIX'			=>'', 			// 数据库表前缀
+		'DB_CHARSET'		=>'utf8', 		// 字符集
+		'DB_FIELDS_CACHE'	=>false,
+		'READ_DATA_MAP'		=>true,
+		
+		'DEFAULT_FILTER'	=>'strip_tags',		// 默认的拦截器
+		'API_KEY'			=>'',				// 设置短信平台的验证码
+		
 		'TMPL_PARSE_STRING' =>array(
-			'_CSS_' => '/public/css/', //css文件地址
-			'_SCRIPT_' => '/public/script/',//js文件地址
+			'_CSS_'			=>'/public/css/', 		// css文件地址
+			'_SCRIPT_' 		=>'/public/script/',	// js文件地址
+			'_ENTER_'		=>'/index.php/home/',	// 入口地址
+			'_ADMIN_'		=>'/index.php/admin/',	// 后台入口
 			
 		),
+		'DEFAULT_C_LAYER'	=>'Controller', 			// 默认的控制器层名称
+		'MODULE_ALLOW_LIST'	=>array('Home','Admin'), 	// 分组列表
+		'DEFAULT_MODULE'	=>'Home', 					// 默认分组
 );
