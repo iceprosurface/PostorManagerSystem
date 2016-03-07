a772542ad2a4acbcad829d44ac789269e9420541
iceprosurface <775846939@qq.com>
2016-03-07 15:48:05 +0800

    description

--- Application/Admin/Controller/EditController.class.php
+++ Application/Admin/Controller/EditController.class.php
@@ -5,13 +5,14 @@ class EditController extends BaseController {
 	function logined(){
 		$this->display(logined);
 	}
-	/*
+	/**
+	*查询模块，用于获取数据
 	*@param where (必要属性) array 关键字段(eq){"0":{key:value}}
 	*@param field (必要属性如果不提交则认为不查询任何) array 需要查询字段，类型为{"0":value,……}
 	*@param from (必要属性) string 查询的表（后端检验）
 	*@param pagination (必要属性) int 分页
 	*@param page int 页码
-	*@return list 
+	*@return list array返回查询列表{"0":{array}……}
 	*/
 	function query(){
 		$where=I("post.where");
